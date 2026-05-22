6ee8d59fbd745c99e753176112a9fbe6c036c276
Chris-medi <ccfranm17@gmail.com>
2026-05-21 19:19:24 -0500

    feat: update dependencies and integrate PrimeNG components
    - Added PrimeNG and PrimeUI themes to package.json.
    - Configured app.module.ts to include PrimeNG's ToastModule and set up global error listeners.
    - Updated app.config.ts to provide PrimeNG configuration and zoneless change detection.
    - Removed placeholder content from app.html and added a toast component.
    - Imported PrimeIcons in styles.css for icon usage.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { MessageService } from 'primeng/api';
+// disponible Nora, Material, Lara
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes, withComponentInputBinding()),
+    provideZonelessChangeDetection(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+    MessageService,
+  ],
 };
