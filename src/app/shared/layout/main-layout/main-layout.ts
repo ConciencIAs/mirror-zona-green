7bc31b3dc78ba1414e31ee5d8813917ca1d33855
Chris-medi <ccfranm17@gmail.com>
2026-05-25 18:37:15 -0500

    feat: implement main layout component with footer and integrate into route configuration

---
+++ src/app/shared/layout/main-layout/main-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Footer } from "@src/app/shared/components/footer/footer";
+import { Navbar } from "@src/app/shared/components/navbar/navbar";
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [RouterOutlet, Footer, Navbar],
+  templateUrl: './main-layout.html',
+})
+export class MainLayout { }
--- src/setup/app.routes.ts
+++ src/setup/app.routes.ts
@@ -1,16 +1,23 @@
 import { Routes } from '@angular/router';
 import { routerAuthCustomer } from '@src/app/features/customer/auth/auth.router';
+import { MainLayout } from '@src/app/shared/layout/main-layout/main-layout';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        title: 'home',
+        loadComponent: () => import('@src/app/features/home/home').then((m) => m.Home),
+      },
+      ...routerAuthCustomer,
+    ]
   },
-  {
-    path: 'home',
-    title: 'home',
-    loadComponent: () => import('@src/app/features/home/home').then((m) => m.Home),
-  },
-  ...routerAuthCustomer,
 ];
