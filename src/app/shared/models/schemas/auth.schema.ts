94f972a7f3763c9895b003deceeec8ad983c6fe9
chris-medi <ccfranm17@gmail.com>
2026-06-11 10:48:57 -0500

    feat: integrate GrapesJS for dynamic page building and update customer registration fields to number types

--- src/app/shared/models/schemas/auth.schema.ts
+++ src/app/shared/models/schemas/auth.schema.ts
@@ -4,9 +4,9 @@ export const userSchemaRegister = z.object({
   full_name: z.string().min(2, 'Nombre demasiado corto (mín. 2 caracteres)'),
   correo: z.email('Ingresa un correo válido').min(1, 'El correo es obligatorio'),
   telefono: z
-    .string()
-    .regex(/^\+?\d{7,15}$/, 'Teléfono inválido (ingresa de 7 a 15 dígitos sin espacios)'),
-  documento: z.string().min(4, 'Documento de identidad no válido (mín. 4 caracteres)'),
+    .number()
+    .min(7, 'Teléfono inválido (ingresa de 7 a 15 dígitos sin espacios)'),
+  documento: z.number().min(4, 'Documento de identidad no válido (mín. 4 caracteres)'),
   fecha_nacimiento: z.date(),
   tipo_documento: z.string().default('CC'),
   ubicacion: z.string().min(2, 'La ubicación provista es demasiado corta'),
